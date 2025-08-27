6590cd0480648f2fa1af49f9c4103b8207e48419
hoan3204 <hoan25913@gmail.com>
2025-08-27 23:48:44 +0700

    Đăng ký ứng viên BE

--- index.ts
+++ index.ts
@@ -1,13 +1,31 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
+import cors from "cors";
+import routes from "./routes/index.route";
+import dotenv from "dotenv";
+import { connectDB } from "./config/database";
+
+// Load biến môi trường
+dotenv.config();
 
 const app = express();
 const port = 4000;
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!');
-});
+// Kết nối DB
+connectDB();
 
+// Cấu hình CORS
+app.use(cors({
+  origin: "http://localhost:3000",
+  methods: ["GET", "POST", "PATCH", "DELETE"],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+// Cho phép gửi data lên dạng json
+app.use(express.json());
+
+// Thiết lập đường dẫn
+app.use("/", routes);
 
 app.listen(port, () => {
-    console.log(`Website dang chay tren cong ${port}`)
+  console.log(`Website đang chạy trên cổng ${port}`);
 });
