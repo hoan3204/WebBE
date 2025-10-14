d2c0cb9c58226324550b4e28faa545335f7fb65c
hoan3204 <hoan25913@gmail.com>
2025-10-14 21:26:17 +0700

    deploy test

--- index.ts
+++ index.ts
@@ -9,14 +9,15 @@ import cookieParser = require('cookie-parser');
 dotenv.config();
 
 const app = express();
-const port = 4000;
+const port = process.env.PORT ? Number(process.env.PORT) : 4000;
+const clientUrl = process.env.CLIENT_URL || 'http://localhost:3000';
 
 // Kết nối DB
 connectDB();
 
 // Cấu hình CORS
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: clientUrl,
   methods: ["GET", "POST", "PATCH", "DELETE"],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true // Cho phép gửi cookie
